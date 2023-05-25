f961e6202461504c46c75c1f67a48dec6c955d43
revdrag <wanghelin67626343@gmail.com>
2023-05-25 19:46:06 +0800

    Added Login and Register Files

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Login } from "./webpages/Login"
+import { Register } from "./webpages/Register"
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App() 
