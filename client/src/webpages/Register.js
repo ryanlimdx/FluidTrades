70eb82644ed5359c49ed6f0384384684753030ec
revdrag <wanghelin67626343@gmail.com>
2023-05-28 00:37:50 +0800

    Worked on backend for login/register feature

--- client/src/webpages/Register.js
+++ client/src/webpages/Register.js
@@ -15,7 +15,7 @@ const Register = () => {
     const handleSubmit = async (values, {setSubmitting}) => {
         try {
             // Make POST request
-            const response = await axios.post('/register', values)
+            const response = await axios.post('http://localhost:4000/register', values)
             console.log(response.data);
         } catch (error) {
             console.log(error);
--- server/server.js
+++ server/server.js
@@ -3,38 +3,29 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
 const User = require('./schemas/User');
 
 // ensure port is up and running
 app.listen(4000, () => {
     console.log("Server started on Port 4000.");
-})
+})  
 
 // connect to database
-mongoose.connect(process.env.DB_URI, {
+mongoose.connect(process.env.DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
     console.log("Connected to database.");
-    mongoose.disconnect();
 }).catch((error) => {
     console.error('Error connecting to MongoDB:', error);;
     process.exit(1);
 });
 
-mongoose.disconnect()
-  .then(() => {
-    console.log('Mongoose connection closed');
-    process.exit();
-  })
-  .catch((error) => {
-    console.error('Error while closing Mongoose connection:', error);
-    process.exit(1);
-});
+
 
 // POST request to register user into database
-app.post('/register', async(req, res) => {
+app.post('http://localhost:4000/register', async(req, res) => {
   try {
     // Create new User
     const newUser = new User(req.body);
@@ -47,6 +38,3 @@ app.post('/register', async(req, res) => {
   }
 })
 
-app.listen(4000, () => {
-  console.log('Server started on port 4000');
-});
