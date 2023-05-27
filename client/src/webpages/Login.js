8af97a233569d174d807ff655d2542ce2069a38e
revdrag <wanghelin67626343@gmail.com>
2023-05-27 15:08:48 +0800

    Improve Login and Register UI

--- client/src/webpages/Login.js
+++ client/src/webpages/Login.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Register from './Register';
+import { Stack, Heading, Text, Center, Button, Input } from '@chakra-ui/react';
+import { Form, Formik } from 'formik';
 
 
 const Login = () => {
+    const loginDetails = {
+
+    }
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -14,17 +18,47 @@ const Login = () => {
     };
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">Email</label>
-                <input value = {email} onChange ={(e) => setEmail(e.target.value)} type="email" placeholder="Email" id="email" name="email"/>
-                <label htmlFor="password">Password</label>
-                <input value = {password} onChange ={(e) => setPassword(e.target.value)} type="password" placeholder="Password" id="password" name="password"/>
-                <button>Log In</button>
-            </form>
+        <Center>
+            <Stack boxShadow="md" bg="whiteAlpha.700" p="20" rounded="md">
+                    <Heading as="h1">Log In</Heading>
+                    <Text fontSize="lg">Please log in with your registered account.</Text>
 
-            <p>Don't have an account? <Link to ="/register">Register here.</Link></p>
-        </>
+                    <Formik
+                        onSubmit={(values, {setSubmitting}) => {
+                            setTimeout(() => {
+                                console.log(values);
+                                setSubmitting(false);
+                            }, 1000);
+                        }}
+                        initialValues={{email: "", password: ""}}          
+                    >
+                        <Form>
+                            <Stack>
+                                <Input name="email" type="email" placeholder="Email"/>
+                                <Input name="password" type="password" placeholder="Password"/>
+                                <Button                             
+                                    isLoading={handleSubmit}
+                                    loadingText="Hang on while we fight the demons."
+                                    size="lg"
+                                    colorScheme="teal"
+                                    type="submit"
+                                    >
+                                    Login
+                                </Button>
+                            </Stack>
+
+                        </Form>
+                    </Formik>
+
+                    
+                    <Stack justify="center" spacing={10}>
+                        <Text as="div" textAlign="center">
+                            Don't have an an account?
+                            <Button as={Link} to="/register" colorScheme="teal" variant="link"> Register here</Button>
+                        </Text>
+                    </Stack>
+            </Stack>
+        </Center>
     );
 };
 
--- server/server.js
+++ server/server.js
@@ -1,22 +1,32 @@
-const express = require("express");
+require('dotenv').config();
+
+const express = require('express');
 const app = express();
 
-// const mongoose = require('mongoose');
-
-// const mongoUri = ""
+const mongoose = require('mongoose');
 
 // ensure port is up and running
-app.listen(3000, () => {
-    console.log("Server started on Port 3000.");
+app.listen(4000, () => {
+    console.log("Server started on Port 4000.");
 })
 
 // connect to database
-// mongoose.connect(mongoUri, {
-//     useNewUrlParser: true
-// }).then(() => {
-//     console.log("Connected to database.")
-//     mongoose.disconnect();
-// }).catch((e) => {
-//     console.log(e)
-// });
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Connected to database.");
+    mongoose.disconnect();
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);;
+    process.exit(1);
+});
 
+mongoose.disconnect()
+  .then(() => {
+    console.log('Mongoose connection closed');
+    process.exit();
+  })
+  .catch((error) => {
+    console.error('Error while closing Mongoose connection:', error);
+    process.exit(1);
+  });
